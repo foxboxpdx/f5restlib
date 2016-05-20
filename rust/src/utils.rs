249fde9f44e8994ea1e58968be8198da7f18de2e
FoxBoxPDX <foxboxpdx@gmail.com>
2016-05-20 05:45:24 +0000

    Util skeleton

---
+++ rust/src/utils.rs
@@ -0,0 +1,16 @@
+// Utility functions
+use hyper::client::Response;
+use hyper::status::StatusCode;
+
+pub fn validate_name(name: &str) -> bool {
+    true
+}
+
+pub fn validate_ipport(ipprt: &str) -> bool {
+    true
+}
+
+pub fn validate_return_code(response: &Response) -> bool {
+    true
+}
+
